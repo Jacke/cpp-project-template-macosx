da484ffa8c57a9b82ced5d59f8e6d8480d27c5c7
Dan T <danwtisdall@gmail.com>
2020-10-01 11:47:57 +0200

    Add includewhatyouuse

---
+++ cmake/includewhatyouuse.cmake
@@ -0,0 +1,14 @@
+cmake_minimum_required(VERSION 3.3 FATAL_ERROR)
+
+find_program(iwyu_path NAMES include-what-you-use iwyu)
+
+if(NOT iwyu_path)
+  message(FATAL_ERROR "Could not find the program include-what-you-use")
+else()
+  message("Found include-what-you-use")
+endif()
+
+function(add_includewhatyouuse EXE_OR_LIB)
+    set_property(${EXE_OR_LIB} PROPERTY CXX_INCLUDE_WHAT_YOU_USE ${iwyu_path})
+endfunction()
+
--- readme.md
+++ readme.md
@@ -10,6 +10,8 @@
 - SFML (must be installed as library, can use homebrew)
 - SPDLOG (must be installed as library)
 - GTest (must be installed as library)
+- Rapidcheck
+- include-what-you-use (must be installed, can use homebrew)
 
 ## Suggested environment is VSCode with the following extensions
 
