9838706f5769cf3e3621dae49aeb96c2f68c77b8
Dan T <danwtisdall@gmail.com>
2020-10-01 11:06:20 +0200

    rapidcheck move

--- tools/compile-proto.sh
+++ tools/compile-proto.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 echo "Compiling .proto files"
-CPP_DST_DIR=src/module/proto;
-CPP_SRC_DIR=src/module/proto;
+CPP_DST_DIR=src/module/libs/proto;
+CPP_SRC_DIR=src/module/libs/proto;
 protoc -I=$CPP_SRC_DIR --cpp_out=$CPP_DST_DIR $CPP_SRC_DIR/protobuffs.proto
-
-
-# Note: proto install configured with './configure CC=clang CXX="clang++ -std=c++11 -stdlib=libc++" CXXFLAGS="-O3" --disable-shared'
