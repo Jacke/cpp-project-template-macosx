dede791a4dea40e5d4c740a9c21115deff1769c4
Dan T <danwtisdall@gmail.com>
2020-10-01 11:16:09 +0200

    rapidcheck

---
+++ cmake/rapidcheck.cmake
@@ -0,0 +1,5 @@
+add_subdirectory("src/module/libs/rapidcheck")
+function(add_rapidcheck EXE_OR_LIB)
+    target_link_libraries(${EXE_OR_LIB} rapidcheck)
+    target_link_libraries(${EXE_OR_LIB} rapidcheck_gtest)
+endfunction()
